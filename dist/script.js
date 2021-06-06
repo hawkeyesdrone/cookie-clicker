e3c6fa174cc4f144d07ebf9c188c7b9f5b906de3
Tim Kung <timothytzkung@gmail.com>
2021-06-06 14:38:12 -0700

    bug fixes

--- dist/script.js
+++ dist/script.js
@@ -3,7 +3,7 @@ const counter = document.getElementById("counter")
 
 let cookieCount = 0;
 let cookiesPerSecond = 0;
-const audio = new Audio('/assets/munch.mp3')
+const audio = new Audio('./assets/munch.mp3')
 
 function addCounter() {
     audio.play()
