f803d5e963f3cdd2350635b425421c730ba17198
Tim Kung <timothytzkung@gmail.com>
2021-06-06 14:23:41 -0700

    initial commit

---
+++ dist/script.js
@@ -0,0 +1,12 @@
+const cookie = document.getElementById('cookie')
+const counter = document.getElementById("counter")
+
+let cookieCount = 0;
+let cookiesPerSecond = 0;
+const audio = new Audio('/assets/munch.mp3')
+
+function addCounter() {
+    audio.play()
+    cookieCount++
+    document.getElementById("counter").innerHTML = cookieCount
+}
